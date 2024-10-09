d2cf62e05df73c5c8289589bd62e2f62b0acdd73
Asleshakan <142888325+Asleshakan@users.noreply.github.com>
2024-10-09 16:58:03 +0530

    Update route.js

--- app/api/auth/[auth0]/route.js
+++ app/api/auth/[auth0]/route.js
@@ -1,12 +1,3 @@
 import { handleAuth } from '@auth0/nextjs-auth0';
 
-export default async function GET(req, res) {
-    // Set cookie headers to clear specific cookies
-    res.setHeader('Set-Cookie', [
-      'auth0.is.authenticated=; Max-Age=0; Path=/; HttpOnly; Secure;', // Clear the Auth0 authentication cookie
-      'your_session_cookie=; Max-Age=0; Path=/; HttpOnly; Secure;', // Clear your custom session cookie, if any
-    ]);
-  
-    // Call Auth0's handleLogout to log out the user from Auth0
-    await handleAuth();
-  }
+export const GET = handleAuth();
