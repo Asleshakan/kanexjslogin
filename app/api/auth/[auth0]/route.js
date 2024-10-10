d4c738a8482b52bf4d65dd5aa63e5fc76d6e79f2
Asleshakan <142888325+Asleshakan@users.noreply.github.com>
2024-10-10 16:16:33 +0530

    Update route.js

--- app/api/auth/[auth0]/route.js
+++ app/api/auth/[auth0]/route.js
@@ -1,5 +1,5 @@
 import { handleAuth, handleLogout } from '@auth0/nextjs-auth0'
 
 export const GET = handleAuth({
-  logout: handleLogout({ returnTo: 'https://ashy-island-0f0ebea10.5.azurestaticapps.net/api/auth/login' }),
+  logout: handleLogout({ returnTo: 'https://ashy-island-0f0ebea10.5.azurestaticapps.net' }),
 })
