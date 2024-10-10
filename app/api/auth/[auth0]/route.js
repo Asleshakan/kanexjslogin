32576ba20e738e3a0a045d34711596faa1e83a4d
Asleshakan <142888325+Asleshakan@users.noreply.github.com>
2024-10-10 16:29:28 +0530

    Update route.js

--- app/api/auth/[auth0]/route.js
+++ app/api/auth/[auth0]/route.js
@@ -1,5 +1,11 @@
-import { handleAuth, handleLogout } from '@auth0/nextjs-auth0'
+import { handleAuth, handleLogin, handleLogout } from '@auth0/nextjs-auth0';
+
+const logoutUrl = [
+  `${process.env.AUTH0_ISSUER_BASE_URL}/v2/logout?`,
+  `client_id=${process.env.AUTH0_CLIENT_ID}`,
+  `&returnTo=${process.env.AUTH0_BASE_URL}`,
+];
 
 export const GET = handleAuth({
-  logout: handleLogout({ returnTo: 'https://ashy-island-0f0ebea10.5.azurestaticapps.net' }),
-})
+  logout: handleLogout({ returnTo: logoutUrl.join('') }),
+});
